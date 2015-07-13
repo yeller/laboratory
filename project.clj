22958937adb38f404394c8567313bc32c62b4401
Tom Crayford <tcrayford@googlemail.com>
2015-07-13 16:18:28 +0100

    add project.clj stuff

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject laboratory "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "do science in production"
+  :url "https://github.com/yeller/laboratory"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
